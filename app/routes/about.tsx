69e2226aed92e017d9545ad0c318a1b66e2ecdf2
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-06-03 11:44:05 -0400

    add basic pages

---
+++ app/routes/about.tsx
@@ -0,0 +1,30 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About me</h1>
+      <img
+        style={{ width: 100, height: 50 }}
+        src="https://images.pexels.com/photos/1618606/pexels-photo-1618606.jpeg?auto=compress&cs=tinysrgb&w=800"
+      ></img>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut eu sem integer
+        vitae justo eget magna fermentum iaculis. Tempus quam pellentesque nec
+        nam aliquam. Tristique magna sit amet purus gravida. Nec feugiat in
+        fermentum posuere. Rutrum tellus pellentesque eu tincidunt tortor
+        aliquam nulla. Cum sociis natoque penatibus et. Nec feugiat in fermentum
+        posuere urna. Morbi tincidunt augue interdum velit euismod in
+        pellentesque massa. Id leo in vitae turpis massa sed elementum tempus.
+        Elementum sagittis vitae et leo duis ut diam. Suspendisse ultrices
+        gravida dictum fusce. Fringilla urna porttitor rhoncus dolor purus.
+      </p>
+      <a href="https://github.com/yayakix" target="_blank">
+        <span>Github</span>
+      </a>
+      <br></br>
+      <a href="https://www.linkedin.com/in/iyana-marquez/" target="_blank">
+        <span>LinkedIn</span>
+      </a>
+    </div>
+  );
+}
---
+++ app/routes/projects.tsx
@@ -0,0 +1,38 @@
+import Header from "~/components/Header";
+
+export default function Projects() {
+  const projects = [
+    {
+      name: "project 1",
+      img: "https://images.pexels.com/photos/776092/pexels-photo-776092.jpeg?auto=compress&cs=tinysrgb&w=800",
+      link: "https://simplecalculator-7d4924.netlify.app/",
+      desc: "ndebfienfle",
+    },
+    {
+      name: "project 2",
+      img: "https://images.pexels.com/photos/207924/pexels-photo-207924.jpeg?auto=compress&cs=tinysrgb&w=800",
+      link: "https://simonsaysgame23422.netlify.app/",
+      desc: "ndebfienfle",
+    },
+  ];
+  return (
+    <div>
+      <Header />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      {projects.map((x) => {
+        return (
+          <div>
+            <div className="grid gap-4 grid-cols-3 grid-rows-3">
+              <h2>{x.name}</h2>
+              <a href={x.link}>Visit</a>
+              <img style={{ width: 150, height: 150 }} src={x.img}></img>
+              <p>{x.desc}</p>
+            </div>
+          </div>
+        );
+      })}
+      <ul></ul>
+    </div>
+  );
+}
